464eb660a84ba567986f48131e189e68629e89ea
dustywoods <dustywoods.zhou@qq.com>
2026-09-14 22:27:09 +0800

    test: refactor all test, and function 'get_parse' passes the tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,3 +1,4 @@
+import sys
 import unittest
 from unittest.mock import patch
 
@@ -5,18 +6,70 @@ from weight.parser import get_args
 
 class TestParser(unittest.TestCase):
 
-    @patch("sys.argv", ["weight", "add", "80.34"])
-    def test_add(self):
-        args = get_args()
+    def test_command(self):
+        test_cases = [
+            {
+                "argv" : [ "weight", "add", "80.32" ],
+                "command" : "add",
+                "weight" : "80.32",
+            },
+            {
+                "argv" : [ "weight", "add", "80.32", "-d", "12-12" ],
+                "command" : "add",
+                "weight" : "80.32",
+                "date" : "12-12",
+            },
+            {
+                "argv" : [ "weight", "add", "80.32", "--date", "12-12" ],
+                "command" : "add",
+                "weight" : "80.32",
+                "date" : "12-12",
+            },
+            {
+                "argv": [ "weight", "add", "-d", "12-12", "80.32"],
+                "command" : "add",
+                "weight" : "80.32",
+                "date" : "12-12",
+            },
+            {
+                "argv": [ "weight", "add", "--date", "12-12", "80.32"],
+                "command" : "add",
+                "weight" : "80.32",
+                "date" : "12-12",
+            },
+            {
+                "argv" : [ "weight", "show"],
+                "command" : "show",
+            },
+            {
+                "argv" : [ "weight", "clean" ],
+                "command" : "clean",
+                "force" : False,
+            },
+            {
+                "argv" : [ "weight", "clean", "-f" ],
+                "command" : "clean",
+                "force" : True,
+            },
+            {
+                "argv" : [ "weight", "clean", "--force" ],
+                "command" : "clean",
+                "force" : True,
+            }
+        ]
 
-        self.assertEqual(args.command, "add")
-        self.assertEqual(args.weight, 80.34)
+        for case in test_cases:
+            with self.subTest(**case):
+                with patch.object(sys, "argv", case["argv"]):
+                    args = get_args()
 
-    @patch("sys.argv", ["weight", "show"])
-    def test_show(self):
-        args = get_args()
-
-        self.assertEqual(args.command, "show")
+                self.assertEqual(args.command, case["command"])
+                if "weight" in case: 
+                    self.assertEqual(args.weight, float(case["weight"]))
+                if "date" in case:
+                    self.assertEqual(args.date, case["date"])
+                if "force" in case:
+                    self.assertEqual(args.force, case["force"])
 
 if __name__ == "__main__":
     unittest.main()
