b0e0c86a6391e73cb8942ec9d8edae7b92bb5a95
dustywoods <dustywoods.zhou@qq.com>
2026-09-15 17:28:33 +0800

    feat: adjust the function 'render' to meet with complex requirement of 'show' command

--- src/weight/utils.py
+++ src/weight/utils.py
@@ -5,30 +5,12 @@ from datetime import datetime
 
 from .models import WeightRecord
 
-def render(records: list[WeightRecord]):
-    date = [ datetime.combine(record.date, datetime.min.time()) for record in records ]
-    weight = [ record.weight for record in records ]
-
-    fig = plt.figure
-    fig.clear()
-
-    fig.theme("colorless")
-
-    fig.plot_size(0.8*shutil.get_terminal_size().columns, 20)
-    fig.date("x").activate(form="%Y-%m-%d")
-    signal = fig.signal(date, weight, marker="dot").lines()
-    fig.draw(signal)
-    fig.title("Recent Weight Record")
-
-    fig.show()
-
-    print()
-    print("Overview:")
-    print(f"\tMaximum: {max(weight):.1f} kg")
-    print(f"\tMinimum: {min(weight):.1f} kg")
-    print(f"\tAverage: {sum(weight) / len(weight):.1f} kg")
-    print(f"\tMedian:  {median(weight):.1f} kg")
-    print()
+def render(records: list[WeightRecord], days = 30, flag_line = True, flag_data = True, flag_graph = True):
+    if flag_graph:
+        _graph(records, flag_line)
+    if flag_data:
+        weights = [ record.weight for record in records ]
+        _data(weights, days)
 
 def confirm_clean(times: int = 3):
     cancelled = False
@@ -50,6 +32,48 @@ def confirm_clean(times: int = 3):
         print("Too many invalid inputs. Operation cancelled.")
 
     return False
+
+def _graph(records, flag_line=True):
+
+    date = [ datetime.combine(record.date, datetime.min.time()) for record in records ]
+    weight = [ record.weight for record in records ]
+
+    fig = plt.figure
+    fig.clear()
+
+    width = shutil.get_terminal_size().columns * 0.8
+    height = 20
+
+    fig.plot_size(width, height)
+    fig.date("x").activate(form="%Y-%m-%d")
+
+    if flag_line:
+        signal1 = fig.signal(date, weight, marker=plt.marker("braille", pixel="white")).lines()
+        fig.draw(signal1)
+
+    signal2 = fig.signal(date, weight, marker=plt.marker("x", pixel="cyon"))
+    fig.draw(signal2)
+
+    fig.theme("simple")
+    fig.title("Recent Weight Record")
+    fig.show()
+
+def _data(weights, n_days):
+    count = len(weights)
+    maximum = max(weights, default=-1)
+    minimum = min(weights, default=-1)
+    average = sum(weights) / count if count else -1
+    med = median(weights) if count else -1
+    change = weights[-1] - weights[-2]
+    print()
+    print(f"Overview in the past {n_days} days ({count} records):")
+    print(f"\tMaximum: {maximum:.1f} kg")
+    print(f"\tMinimum: {minimum:.1f} kg")
+    print(f"\tAverage: {average:.1f} kg")
+    print(f"\tMedian:  {med:.1f} kg")
+    print()
+    print(f"Compared with the previous record, change: {change:.1f} kg")
+    print()
 
 if __name__ == "__main__":
     import random
