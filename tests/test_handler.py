50f5628569885c3e74ca4c2568f657799d2ff30b
dustywoods <dustywoods.zhou@qq.com>
2026-09-15 11:19:09 +0800

    test: refactor all tests in test_handler.py, and pass the tests

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,21 +1,107 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
+from datetime import date
+from argparse import Namespace
 
 from weight.handler import Handler
 
 class TestHandler(unittest.TestCase):
 
+    def setUp(self):
+        self.repository = Mock()
+        self.handler = Handler(self.repository)
+
     def test_add(self):
-        repository = Mock()
-        handler = Handler(repository)
+        test_cases = [
+            {
+                "weight" : 80.99,
+                "_date" : date.today(),
+            },
+            {
+                "weight" : 80.99,
+                "date" : "2022-12-12",
+                "_date" : date(2022, 12, 12),
+            },
+        ]
 
-        args = Mock()
-        args.command = "add"
-        args.weight = 70.38
+        for case in test_cases:
+            with self.subTest(**case):
+                self.repository.reset_mock()
 
-        handler.handle(args)
+                args = Namespace(
+                    command = "add",
+                    weight = case["weight"],
+                    date = case['date'] if 'date' in case else None
+                )
 
-        repository.add_today.assert_called_once_with(70.38)
+                self.handler.handle(args)
+
+                self.repository.add.assert_called_once_with(case["_date"], case["weight"])
+
+    @patch("weight.handler.render")
+    def test_show(self, mock_render):
+        records = []
+        self.repository.recent.return_value = records
+
+        args = Namespace(
+            command = "show"
+        )
+
+        self.handler.handle(args)
+
+        self.repository.recent.assert_called_once_with()
+        mock_render.assert_called_once_with(records)
+
+    @patch("weight.handler.confirm_clean")
+    def test_clean(self, mock_confirm_clean):
+        test_cases = [
+            {
+                "force" : False,
+                "confirm" : True,
+            },
+            {
+                "force" : False,
+                "confirm" : False,
+            },
+            {
+                "force" : True,
+                "confirm" : True,
+            },
+            {
+                "force" : True,
+                "confirm" : False,
+            }
+        ]
+
+        print_value = "All records deleted"
+
+        for case in test_cases:
+            with self.subTest(**case):
+                self.repository.reset_mock()
+                mock_confirm_clean.reset_mock()
+
+                args = Namespace(
+                    command = "clean",
+                    force = case["force"],
+                )
+                mock_confirm_clean.return_value = case['confirm']
+
+                with patch("builtins.print") as mock_print:
+                    self.handler.handle(args)
+                if args.force:
+                    mock_confirm_clean.assert_not_called()
+
+                    self.repository.clean.assert_called_once_with()
+                    mock_print.assert_called_once_with(print_value)
+                else:
+                    mock_confirm_clean.assert_called_once_with()
+
+                    if case['confirm']:
+                        self.repository.clean.assert_called_once_with()
+                        mock_print.assert_called_once_with(print_value)
+                    else:
+                        self.repository.clean.assert_not_called()
+                        mock_print.assert_not_called()
 
 if __name__ == "__main__":
     unittest.main()
