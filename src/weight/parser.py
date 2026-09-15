1988bec395a8ad60c2964bb5205ab7ab8c970c06
dustywoods <dustywoods.zhou@qq.com>
2026-09-15 16:08:19 +0800

    style: parser.py

--- src/weight/parser.py
+++ src/weight/parser.py
@@ -2,68 +2,68 @@ import argparse
 
 def create_parser():
     parser = argparse.ArgumentParser(
-            description="Weight Tracking Tool"
-            )
+        description="Weight Tracking Tool"
+    )
 
     subparsers = parser.add_subparsers(
-            dest="command",
-            required=True
-            )
+        dest="command",
+        required=True
+    )
 
     add_parser = subparsers.add_parser(
-            "add",
-            help="Add today's weight record (kg; rounded to one decimal place"
-            )
+        "add",
+        help="Add today's weight record (kg; rounded to one decimal place"
+    )
     add_parser.add_argument(
-            "weight",
-            type=float
-            )
+        "weight",
+        type=float
+    )
     add_parser.add_argument(
-            "-d",
-            "--date",
-            type=str,
-            help="Record date in YYYY-MM-DD format (default: today)"
-            )
+        "-d",
+        "--date",
+        type=str,
+        help="Record date in YYYY-MM-DD format (default: today)"
+    )
 
     show_parser = subparsers.add_parser(
-            "show",
-            help="Display the past 30 days of weight records using a line chart"
-            )
+        "show",
+        help="Display the past 30 days of weight records using a line chart"
+    )
     show_parser.add_argument(
-            "-r",
-            "--recent",
-            type=int,
-            default=30,
-            metavar="N",
-            help="Show records from the recent N days (default: 30)"
-            )
+        "-r",
+        "--recent",
+        type=int,
+        default=30,
+        metavar="N",
+        help="Show records from the recent N days (default: 30)"
+    )
     show_parser.add_argument(
-            "--no-lines",
-            action="store_true",
-            help="Hide line in the graph",
-            )
+        "--no-lines",
+        action="store_true",
+        help="Hide lines in the graph",
+    )
     show_parser.add_argument(
-            "--no-data",
-            action="store_true",
-            help="Do not output the statistical data"
-            )
+        "--no-data",
+        action="store_true",
+        help="Do not output the statistical data"
+    )
     show_parser.add_argument(
-            "--no-graph",
-            action="store_true",
-            help="Do not output the graph"
-            )
+        "--no-graph",
+        action="store_true",
+        help="Do not output the graph"
+    )
 
 
     clean_parser = subparsers.add_parser(
-            "clean",
-            help="Clear all weight records"
-            )
+        "clean",
+        help="Clear all weight records"
+    )
     clean_parser.add_argument(
-            "-f",
-            "--force",
-            action="store_true",
-            help="CLear all weight records without confirmation"
-            )
+        "-f",
+        "--force",
+        action="store_true",
+        help="CLear all weight records without confirmation"
+    )
 
     return parser
 
